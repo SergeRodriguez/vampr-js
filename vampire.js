3e17164b2e4d34d75886f0afc890a05b0277d708
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-11-01 19:33:31 +0000

    created a vampire class which has different properties and methods

--- vampire.js
+++ vampire.js
@@ -8,24 +8,33 @@ class Vampire {
 
   /** Simple tree methods **/
 
+
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -36,9 +45,33 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    if (this === vampire) {
+      return vampire;
+    }
 
+    if (!vampire.creator) {
+      return vampire;
+    }
+
+    if (!this.creator) {
+      return this;
+    }
+
+    if (this.isMoreSeniorThan(vampire)) {
+      return this.closestCommonAncestor(vampire.creator);
+    } else {
+      return vampire.closestCommonAncestor(this.creator);
+    }
+
+    //  if(thisVampire !== vampire.creator && vampire.creator!== null){
+    //    return thisVampire.closestCommonAncestor(vampire.creator)
+    //  }
+
+    //  if (vampire !== thisVampire && thisVampire.creator !== null ){
+    //   return vampire.closestCommonAncestor(thisVampire.creator)
+    //  }
+  
   }
 }
-
 module.exports = Vampire;
 
